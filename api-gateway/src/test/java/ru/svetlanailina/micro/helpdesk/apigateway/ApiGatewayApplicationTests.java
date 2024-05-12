b08d24ed1966b2d0e53d60568ccfa29660cd76b9
svetlanailina <developersvil@gmail.com>
2024-05-12 17:56:09 +0300

    eureka-client api-gateway перенаправление  запроса на микросервис http://localhost:8082/main/test

---
+++ api-gateway/src/test/java/ru/svetlanailina/micro/helpdesk/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.svetlanailina.micro.helpdesk.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
